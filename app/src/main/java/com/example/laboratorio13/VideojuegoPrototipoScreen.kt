f7e3573f6f5f8b046cb4733a9ecc58c339e94b09
Anyelinayolit <anyelina.mamani@tecsup.edu.pe>
2026-06-09 16:40:09 -0500

    creacion ejercicio final videojuego

---
+++ app/src/main/java/com/example/laboratorio13/VideojuegoPrototipoScreen.kt
@@ -0,0 +1,8 @@
+package com.example.laboratorio13 // Ajusta a tu paquete
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun VideojuegoPrototipoScreen() {
+    // Base inicial para el prototipo del juego
+}
