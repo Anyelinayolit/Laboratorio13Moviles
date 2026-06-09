02de229280531cddb4f9a5c23b0bcde052e0e050
Anyelinayolit <anyelina.mamani@tecsup.edu.pe>
2026-06-09 16:32:24 -0500

    creacion ejercicio 4

---
+++ app/src/main/java/com/example/laboratorio13/Ejercicio4Screen.kt
@@ -0,0 +1,8 @@
+package com.example.laboratorio13 // Ajusta a tu paquete
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Ejercicio4Screen() {
+    // Base inicial para el ejercicio 4
+}
